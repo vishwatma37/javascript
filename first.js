4c7e9ef526be3d712831699f29b847a7010df1a7
parmatmaau <parmatma.yadavau@gmail.com>
2025-07-29 10:03:34 +0530

    string

--- first.js
+++ first.js
@@ -1,12 +1,7 @@
-let gameNum = 25;
+let str = "vishwatma";
+console.log(str);
 
-let userNum = prompt("Guess the game number : ");
-
-while (userNum != gameNum) {
-    userNum = prompt("you entered wrong number. Guess again : ");
-}
-
-console.log("congratulations, you entered the right number ");
-
-
-
+let str2 = 'vishwatma';
+console.log(str2);
+console.log(str2.length);
+console.log(str2[0], str2[1], str2[2], str2[3], str2[4], str2[5], str2[6], str2[7], str2[8]);
