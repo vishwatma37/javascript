7b82ff42c85b1ee6132dedd9a0f0ffb7c6785dcb
parmatmaau <parmatma.yadavau@gmail.com>
2025-07-29 06:58:38 +0530

    loops

--- first.js
+++ first.js
@@ -1,5 +1,30 @@
-let i = 1;
-while (i <= 5) {
-    console.log("i=", i);
-    i++;
+// // let i = 1;
+// // do {
+// //     console.log("apna college");
+// //     i++;
+// // } while (i <= 5);
+    
+// // for -of loop
+// let str = "Javascript";
+
+
+// let size = 0;
+// for (let i of str) {
+//     console.log("i=", i);
+//     size++;
+// }
+
+// console.log("size of string is", size);
+
+
+let student = {
+    name: "Rahul kumar",
+    age: 20,
+    cgpa:7.5,
+    isPass:true,
+
+};
+
+for(let key in student) {
+    console.log("key=", key, "value=", student[key]);
 }
