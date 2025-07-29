46be13a542d831ffcf32e8431500cc6d190bde44
parmatmaau <parmatma.yadavau@gmail.com>
2025-07-29 10:42:30 +0530

    template literals

--- first.js
+++ first.js
@@ -1,16 +1,4 @@
-let object = {
-    item: "pen",
-    price: 10,
-};
-
-let output = `the cost of ${object.item} is ${object.price} rupees`;
-console.log(output);
-
-console.log("The cost of", object.item, "is", object.price, "rupees");
-
-
-
 // Template Literals
-let speicalString = `this is a template literal`
-console.log(speicalString);
-console.log(typeof(speicalString));
+let specialString = `this is a temaplate literal ${1 + 2 + 3}`;
+console.log(typeof(specialString)); 
+console.log(specialString);
