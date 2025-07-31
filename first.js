fd78565bec0562db003d557b9d232b891f13a907
parmatmaau <parmatma.yadavau@gmail.com>
2025-07-31 12:00:14 +0530

    for

--- first.js
+++ first.js
@@ -1,6 +1,10 @@
-let marks = [94,43,45,43,34];
-console.log(marks);
 
 
 let heroes = ["vish1","vish2","vish3","vish4","vish5"];
-console.log(heroes);
+// for loop
+for(let i=0; i<heroes.length; i++){
+    console.log(heroes[i]);
+}
+
+//for of
+
