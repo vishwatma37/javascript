b01081770ca37db6304bb62d5473bfabe0c7188f
parmatmaau <parmatma.yadavau@gmail.com>
2025-07-31 11:26:30 +0530

    arry

--- first.js
+++ first.js
@@ -1,5 +1,6 @@
-let fullName = prompt("Enter your full name without spaces:");
+let marks = [94,43,45,43,34];
+console.log(marks);
 
-let username = "@" + fullName + fullName.length;
-console.log("Your username is: " + username);
 
+let heroes = ["vish1","vish2","vish3","vish4","vish5"];
+console.log(heroes);
