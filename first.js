852ec0ac77108584f738e232971b145e52965cba
parmatmaau <parmatma.yadavau@gmail.com>
2025-07-31 14:35:11 +0530

    slice

--- first.js
+++ first.js
@@ -1,5 +1,5 @@
-let marvelHeros = ["thor", "spiderman","ironman"];
-let dcHeroes = ["superman", "batman"];
-let indianHero = ["srk","ajay"];
-let heroes = marvelHeros.concat(dcHeroes,indianHero);
-console.log(heroes);
+let marvelHeroes = ["thor","spiderman","ironman","antman","Dr.Strang"];
+
+console.log(marvelHeroes);
+
+console.log(marvelHeroes.slice(1,3));
