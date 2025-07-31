500333ecfa03520c718e87416c6e80ffc39cd758
parmatmaau <parmatma.yadavau@gmail.com>
2025-07-31 14:22:04 +0530

    concat

--- first.js
+++ first.js
@@ -1,4 +1,5 @@
-let foodItems = ["potato","apple","litchi","tomato"]
-
-console.log(foodItems);
-console.log(foodItems.toString());
+let marvelHeros = ["thor", "spiderman","ironman"];
+let dcHeroes = ["superman", "batman"];
+let indianHero = ["srk","ajay"];
+let heroes = marvelHeros.concat(dcHeroes,indianHero);
+console.log(heroes);
