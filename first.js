999940357177576aaf1aaac5ece2501c0fcca99f
parmatmaau <parmatma.yadavau@gmail.com>
2025-07-31 13:11:17 +0530

    practice

--- first.js
+++ first.js
@@ -1,10 +1,41 @@
+let cities = ["delhi", "puna", "mumbai", "gurgaon"];
 
-
-let heroes = ["vish1","vish2","vish3","vish4","vish5"];
-// for loop
-for(let i=0; i<heroes.length; i++){
-    console.log(heroes[i]);
+for (let city of cities){
+    // console.log(city);
+    console.log(city.toUpperCase())
 }
 
-//for of
 
+
+
+let marks = [85,97,44,37,76,60];
+
+let sum = 0;
+
+for (let mark of marks){
+    sum += mark;
+// console.log(mark);
+}
+
+let avg = sum / marks.length;
+console.log(`avg marks of the class = ${avg}` );
+
+let items = [250,645,300,900,50];
+
+let i = 0;
+for (let val of items){
+    let offer = val / 10;
+    items[i] = items[i] - offer;
+    console.log(`value after offer = ${items[i]}`);
+    i++;
+}
+
+
+let items = [250,645,300,900,50];
+
+for (let i = 0; i < items.length; i++) {
+   let offer = items[i] / 10;
+   items[i] -= offer;
+}
+
+console.log(items);
