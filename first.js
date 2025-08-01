438403b4d26889a92e39357306d5aa0e0da9da4c
parmatmaau <parmatma.yadavau@gmail.com>
2025-08-01 08:34:50 +0530

    arrowfunctin

--- first.js
+++ first.js
@@ -1,32 +1,23 @@
-function myFunction() {
-    console.log("welcome to Apna College!")
-    console.log("welcome to Apna College!")
-    console.log("welcome to Apna College!")
+//multiplication function
+function mul (a,b) {
+    return a * b;
 }
 
-myFunction();
-myFunction();
+mul(3,4);
 
-function myFunction(msg) {
-    //parameter -> input
-    console.log(msg);
-}
+const arrowMul = (a,b) => {
+    console.log(a * b);
+};
 
-myFunction("I love you"); //argument
-
-
-// function -> 2 numbers, sum
+// sum function
 
 function sum(a,b) {
+    return a + b;
+}
+
+sum(3,4)
+
+const arrowSum = (a,b) => {
     console.log(a + b);
-}
+};
 
-sum(12,12);
-
-function sum(x,y) {
-    // local variables -> scope
-    s = x + y;
-    return s;
-}
-let val = sum(3,4);
-console.log(val);
