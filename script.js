f2ee4cf02fc5e46774a6cf1a9f69fba53d1aa928
parmatmaau <parmatma.yadavau@gmail.com>
2025-08-02 16:50:24 +0530

    javascript

---
+++ script.js
@@ -0,0 +1,52 @@
+// let heading = document.getElementsByClassName("heading")
+// console.log(heading);
+// console.dir(heading);
+
+// selecting with id 
+// document.getElementById("myId")
+
+// selecting with tag 
+
+//  let h1 = document.getElementsByTagName("h1");
+// console.dir(h1);
+
+
+
+// Query Selector 
+// document.querySelector("myid/myclass/tag")
+
+// let elements = document.querySelector("h1");
+// console.dir(elements);
+
+// let firstEl = document.querySelector("h1");
+// console.dir(firstEl);
+
+// let allEl = document.querySelectorAll("h1");
+// console.dir(allEl);
+
+
+// let firstEl = document.querySelector(".heading");
+// console.dir(firstEl);
+
+// let allEl = document.querySelectorAll(".heading");
+// console.dir(allEl);
+
+
+// let firstEl = document.querySelector("#heading");
+// console.dir(firstEl);
+
+// let allEl = document.querySelectorAll("#heading");
+// console.dir(allEl);
+
+// parent=>child=>sibling ==>DOM
+
+
+// let firstEl = document.querySelector("h1");
+// console.dir(firstEl);
+
+let div = document.querySelector("div");
+console.log(div);
+console.dir(div);
+
+let heading = document.querySelector("h1");
+console.dir(heading);
