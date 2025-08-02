aaa68e481a1dc4220ee7b1e9a18c4734b74bfe99
parmatmaau <parmatma.yadavau@gmail.com>
2025-08-02 17:35:50 +0530

    practice

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-let h2 = document.querySelector("h2");
-console.dir(h2);
+// let h2 = document.querySelector("h2");
+// console.dir(h2);
 
-console.dir(h2.innerText);
+// console.dir(h2.innerText);
 
-h2.innerText = h2.innerText + " from Apna College students";
+// h2.innerText = h2.innerText + " from Apna College students";
+
+// let divs = document.querySelectorAll(".box");
+// // console.log(divs);
+// let idx = 1;
+// for (div of divs) {
+//     // console.log(div.innerText);
+//     div.innnerText = `new unique value ${idx}`;
+//     idx++;
+// }
+
+
+// console.log(divs[0]);
+// divs[0].innerText = "new unique value 1";
+// divs[2].innerText = "new unique value 3";
+
+
+let divs = document.querySelectorAll(".box");
+let idx = 1;
+for (div of divs) {
+    div.innerText = `new unique value ${idx}`;
+    idx++;
+}
