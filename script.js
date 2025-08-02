af8e1eebe0b3190bba8428cae816a03dbfb48d08
parmatmaau <parmatma.yadavau@gmail.com>
2025-08-02 17:04:22 +0530

    practice

--- script.js
+++ script.js
@@ -1,52 +1,6 @@
-// let heading = document.getElementsByClassName("heading")
-// console.log(heading);
-// console.dir(heading);
+let h2 = document.querySelector("h2");
+console.dir(h2);
 
-// selecting with id 
-// document.getElementById("myId")
+console.dir(h2.innerText);
 
-// selecting with tag 
-
-//  let h1 = document.getElementsByTagName("h1");
-// console.dir(h1);
-
-
-
-// Query Selector 
-// document.querySelector("myid/myclass/tag")
-
-// let elements = document.querySelector("h1");
-// console.dir(elements);
-
-// let firstEl = document.querySelector("h1");
-// console.dir(firstEl);
-
-// let allEl = document.querySelectorAll("h1");
-// console.dir(allEl);
-
-
-// let firstEl = document.querySelector(".heading");
-// console.dir(firstEl);
-
-// let allEl = document.querySelectorAll(".heading");
-// console.dir(allEl);
-
-
-// let firstEl = document.querySelector("#heading");
-// console.dir(firstEl);
-
-// let allEl = document.querySelectorAll("#heading");
-// console.dir(allEl);
-
-// parent=>child=>sibling ==>DOM
-
-
-// let firstEl = document.querySelector("h1");
-// console.dir(firstEl);
-
-let div = document.querySelector("div");
-console.log(div);
-console.dir(div);
-
-let heading = document.querySelector("h1");
-console.dir(heading);
+h2.innerText = h2.innerText + " from Apna College students";
